413dc2a7637b39a559d2425982ee386f768c6d34
k-ganda <116561806+k-ganda@users.noreply.github.com>
2023-08-18 13:17:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const inputForm = document.getElementById("input-form");
-  const hotelSummaryContainer = document.getElementById("hotel-summary");
-
-  inputForm.addEventListener("submit", async function (event) {
-    event.preventDefault();
+document
+  .getElementById("input-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault(); // Prevent the form from submitting normally
 
     const domain = document.getElementById("domain").value;
     const locale = document.getElementById("locale").value;
     const hotelId = document.getElementById("hotel-id").value;
 
-    const url = `https://hotels-com-provider.p.rapidapi.com/v2/hotels/summary`;
-    const params = new URLSearchParams({
-      domain: domain,
-      locale: locale,
-      hotel_id: hotelId,
-    });
+    const url = `https://hotels-com-provider.p.rapidapi.com/v2/hotels/summary?domain=${domain}&locale=${locale}&hotel_id=${hotelId}`;
 
     const options = {
       method: "GET",
       headers: {
-        "X-RapidAPI-Key": "cfeeaad2e6mshad34879bffb5ca5p1765a1jsnd66f9515df31",
+        "X-RapidAPI-Key": "c2e3fe64a2msh77c749c5e8811acp1df9c3jsn291fdd37d9df",
         "X-RapidAPI-Host": "hotels-com-provider.p.rapidapi.com",
       },
     };
 
-    const response = await fetch(`${url}?${params}`, options);
-    const data = await response.json();
-    console.log(data);
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json(); // Parse the JSON response
 
-    hotelSummaryContainer.innerHTML = `
-              <h3>Hotel Summary</h3>
-              <p>Hotel Name: ${data.name}</p>
-              <p>Address: ${data.address}</p>
-              <p>Rating: ${data.rating}</p>
-              <p>Description: ${data.description}</p>
-          `;
+      // Format the specific properties for display
+      const hotelName = data.summary.name;
+      const tagline = data.summary.tagline;
+      const rating = data.summary.overview.propertyRating.rating;
+      const amenities = data.summary.amenities.topAmenities.items
+        .map((item) => item.text)
+        .join(", ");
+
+      // Create the formatted HTML
+      const formattedData = `
+      <p>Hotel Name: ${hotelName}</p>
+      <p>Tagline: ${tagline}</p>
+      <p>Rating: ${rating}</p>
+      <p>Amenities: ${amenities}</p>
+    `;
+
+      console.log(data); // Log the parsed data
+      // Display the formatted data on your website
+      document.getElementById("hotel-summary").innerHTML = formattedData;
+    } catch (error) {
+      console.error(error);
+    }
   });
-});
